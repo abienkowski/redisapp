79253d922814c659167804d9eeac5b871dc1caca
Adrian Bienkowski <abienkow@uwo.ca>
2022-12-13 11:54:30 -0500

    fix pull_request type

--- app/routes/healthz.js
+++ app/routes/healthz.js
@@ -3,10 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(products);
-});
-
 router.get('/healthz', (req, res, next) => {
   const statusOK = {
     "status": "OK"
