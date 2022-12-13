c3e11e455caf08a9c58ca41f7d7deafdef4eab60
Adrian Bienkowski <abienkow@uwo.ca>
2022-12-13 08:49:28 -0500

    initial podinfo app

---
+++ app/app.js
@@ -0,0 +1,13 @@
+// -- --
+// -- app.js
+const express = require('express');
+const healthz = require('./routes/healthz');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use('/', healthz);
+
+module.exports = app;
---
+++ app/routes/healthz.js
@@ -0,0 +1,26 @@
+// -- --
+// -- healthz.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json(products);
+});
+
+router.get('/healthz', (req, res, next) => {
+  const statusOK = {
+    "status": "OK"
+  }
+
+  res.json(statusOK);
+});
+
+router.get('/readyz', (req, res, next) => {
+  const statusOK = {
+    "status": "OK"
+  }
+
+  res.json(statusOK);
+});
+
+module.exports = router;
